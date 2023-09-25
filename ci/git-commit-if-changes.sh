4170b7617a18d9a2bda53a46a5888157a20c904c
Aleh Strakachuk <o.strokachuk@strikerz.inc>
2023-09-25 23:08:06 +0300

    Add only data folder

--- ci/git-commit-if-changes.sh
+++ ci/git-commit-if-changes.sh
@@ -3,12 +3,14 @@ set -e
 
 BRANCH="$(git branch --show-current)"
 COMMIT="$(git rev-parse --short HEAD)"
+PWD="$(pwd)"
 
-echo "Check changes for ${BRANCH} on commit ${COMMIT}"
+echo "Check changes for ${BRANCH} on commit ${COMMIT} in ${PWD}"
 if [ $(git status --porcelain | wc -l) -eq "0" ]; then
   echo "  🟢 Git repo is clean."
 else
   echo "  🔴 Git repo has changes. Commit"
-  git commit -am "Commit to ${BRANCH} changes for because of changes in ${COMMIT}"
+  git add data
+  git commit -m "Commit to ${BRANCH} changes for because of changes in ${COMMIT}"
   git push origin ${BRANCH}
 fi
