cfb9b6d279e43d6cd61ada226f7034d10198efcf
Aleh Strakachuk <o.strokachuk@strikerz.inc>
2023-09-25 23:10:11 +0300

    Continue fixes update job

--- ci/git-commit-if-changes.sh
+++ ci/git-commit-if-changes.sh
@@ -6,6 +6,8 @@ COMMIT="$(git rev-parse --short HEAD)"
 PWD="$(pwd)"
 
 echo "Check changes for ${BRANCH} on commit ${COMMIT} in ${PWD}"
+git status
+
 if [ $(git status --porcelain | wc -l) -eq "0" ]; then
   echo "  🟢 Git repo is clean."
 else
